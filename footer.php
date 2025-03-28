ca683acd3f750cc4e18a1ed6e9544f8556e857c5
ryuzaki999 <naraphat999999@gmail.com>
2025-03-28 18:01:16 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>ลิงค์ด่วน</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>ลิงก์เสริม</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> รถเข็น</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> เข้าสู่ระบบ</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> ลงทะเบียน</a>
+      </div>
+
+      <div class="box">
+         <h3>ข้อมูลการติดต่อ</h3>
+         <p> <i class="fas fa-phone"></i> 093-467-0019 </p>
+         <p> <i class="fas fa-phone"></i> 063-891-1027 </p>
+         <p> <i class="fas fa-envelope"></i> naraphat@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Phitsanulok, Thai - 65000 </p>
+      </div>
+
+      
+
+   </section>
+
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?>  | all rights reserved! </p>
+
+</footer>

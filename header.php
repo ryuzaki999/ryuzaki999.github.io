9395fac96233a1f6e0473bc0621cc72da13edebc
ryuzaki999 <naraphat999999@gmail.com>
2025-03-28 17:36:17 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,62 @@
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+
+?>
+
+<header class="header">
+
+   <div class="flex">
+   <img src="images/icon2.jpg" width="50" height="50">
+      <a href="home.php" class="logo">Basu Ketto<span>.</span></a>
+
+      <nav class="navbar">
+         <a href="home.php">home</a>
+         <a href="shop.php">shop</a>
+         <a href="orders.php">orders</a>
+         <a href="about.php">about</a>
+         <a href="contact.php">contact</a>
+      </nav>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <a href="search_page.php" class="fas fa-search"></a>
+         <?php
+            $count_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+            $count_cart_items->execute([$user_id]);
+            $count_wishlist_items = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id = ?");
+            $count_wishlist_items->execute([$user_id]);
+         ?>
+         
+         <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?= $count_cart_items->rowCount(); ?>)</span></a>
+      </div>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <img src="uploaded_img/<?= $fetch_profile['image']; ?>" alt="">
+         <p><?= $fetch_profile['name']; ?></p>
+         <a href="user_profile_update.php" class="btn">อัปเดตโปรไฟล์</a>
+         <a href="logout.php" class="delete-btn">ออกจากระบบ</a>
+         <div class="flex-btn">
+            <a href="login.php" class="option-btn">เข้าสู่ระบบ</a>
+            <a href="register.php" class="option-btn">ลงทะเบียน</a>
+         </div>
+      </div>
+
+   </div>
+
+</header>
